9eefaec2bb0e9fd47facda01fbffb60391233d10
unknown <jananjeevithas2007@gmail.com>
2026-01-25 20:58:19 +0530

    fix cors

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
-const cors = require("cors")
+
 
 const app = express()
-app.use(cors())
 app.use(express.json())
 app.use("/uploads", express.static("uploads"))
 
