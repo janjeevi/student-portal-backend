29cb1a93fba4fa0eef734f6d46d81e7e29d98bff
unknown <jananjeevithas2007@gmail.com>
2026-01-25 21:02:55 +0530

    final cors fix

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-require("dotenv").config()
-const express = require("express")
-const mongoose = require("mongoose")
-
-
-const app = express()
-app.use(express.json())
-app.use("/uploads", express.static("uploads"))
 
 
 // ================= MONGO =================
