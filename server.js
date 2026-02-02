84a55cd43a6550e66e57e8380b4850f0051e6504
Jan Jimin <janjimin@gmail.com>
2026-02-02 16:26:11 +0530

    fix file upload folder

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const multer = require("multer")
+const fs = require("fs")
 const path = require("path")
 
 // ================= APP =================
@@ -25,7 +26,8 @@ app.use(cors({
 app.options("*", cors())
 
 app.use(express.json())
-app.use("/uploads", express.static("uploads"))
+app.use("/uploads", express.static(path.join(__dirname, "uploads")))
+
 console.log("🔥 server.js loaded")
 
 // ================= MONGO CONNECT =================
@@ -140,11 +142,14 @@ const File = mongoose.model(
   })
 )
 
+const uploadDir = path.join(__dirname, "uploads")
 
-
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir)
+}
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads")
+    cb(null, uploadDir)
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname)
@@ -152,6 +157,8 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage })
+
+
 
 // ================= TEST =================
 app.get("/ping", (req, res) => {
